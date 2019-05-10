b1b4c9c7f7af3e288df5b1563220c5234584a15e
je-eonpark <ggungtoo@icloud.com>
2019-05-10 20:38:25 +0900

    Start Acitvity 생성

--- app/src/main/java/com/waitwait/StartActivity.java
+++ app/src/main/java/com/waitwait/StartActivity.java
@@ -1,5 +1,6 @@
 package com.waitwait;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,14 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+
+        try{
+            Thread.sleep(3000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+
     }
 }
