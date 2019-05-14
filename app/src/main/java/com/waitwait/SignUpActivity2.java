82254148a6082657fca056d43a97c06da6ddaf90
je-eonpark <ggungtoo@icloud.com>
2019-05-14 21:27:14 +0900

    올림

---
+++ app/src/main/java/com/waitwait/SignUpActivity2.java
@@ -0,0 +1,13 @@
+package com.waitwait;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up2);
+    }
+}
