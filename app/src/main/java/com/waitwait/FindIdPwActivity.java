ecacb01924b9ff8cb262a556048044a8c4f7874c
조윤성 <jys991@naver.com>
2019-05-22 02:34:02 +0900

    Merge branch 'master' of E:\Mobile\WaitWait with conflicts.

--- app/src/main/java/com/waitwait/FindIdPwActivity.java
+++ app/src/main/java/com/waitwait/FindIdPwActivity.java
@@ -1,5 +1,6 @@
 package com.waitwait;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class FindIdPwActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_id_pw);
+
+
     }
 }
