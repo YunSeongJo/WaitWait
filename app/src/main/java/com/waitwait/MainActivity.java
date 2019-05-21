ce56f88485cd78bfabd8ac25a2a7d8ee224daa4e
je-eonpark <ggungtoo@icloud.com>
2019-05-22 03:03:07 +0900

    로그인 라벨 구현

--- app/src/main/java/com/waitwait/MainActivity.java
+++ app/src/main/java/com/waitwait/MainActivity.java
@@ -3,6 +3,8 @@ package com.waitwait;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -10,7 +12,16 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+
+    // FrameLayout에 각 메뉴의 Fragment를 바꿔 줌
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    // 4개의 메뉴에 들어갈 Fragment들
+    private HomeFragment homeFragment = new HomeFragment();
+    //private Menu2Fragment menu2Fragment = new Menu2Fragment();
+    //private Menu3Fragment menu3Fragment = new Menu3Fragment();
+
+    FragmentTransaction transaction = fragmentManager.beginTransaction();
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,13 +30,11 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    transaction.replace(R.id.frame_layout, homeFragment).commitAllowingStateLoss();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -37,8 +46,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // 첫 화면 지정
+        transaction.replace(R.id.frame_layout, homeFragment).commitAllowingStateLoss();
     }
 
 
